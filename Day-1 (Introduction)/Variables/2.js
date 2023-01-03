ae54611d3d75e552211b6e6c33ca552ef66a6d12
Shivam Kumar <89576755+shivam1141@users.noreply.github.com>
2023-01-03 20:24:22 +0530

    Add files via upload

---
+++ Day-1 (Introduction)/Variables/2.js
@@ -0,0 +1,19 @@
+console.log("Hello! This is a program for simple calculator using the variables")
+let a = 45
+let b = 20
+
+//Program for addition
+var c = a+b
+console.log(c)
+
+//Program for subtraction
+var c = a-b
+console.log(c)
+
+//Program for multiplication
+var c = a*b
+console.log(c)
+
+//Program for division
+var c = a/b
+console.log(c)
