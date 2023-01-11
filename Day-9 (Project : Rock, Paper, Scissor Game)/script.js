9fd863888967713f054f6ab3d4a2a7914eb1e09a
Shivam Kumar <89576755+shivam1141@users.noreply.github.com>
2023-01-11 23:28:24 +0530

    Add files via upload

---
+++ Day-9 (Project : Rock, Paper, Scissor Game)/script.js
@@ -0,0 +1,25 @@
+function playRPS(playerChoice) {
+  // Array of possible computer choices
+  const computerChoices = ["rock", "paper", "scissors"];
+  
+  // Generate a random choice for the computer
+  const computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  
+  // Determine the outcome of the game
+  let result;
+  if (playerChoice === computerChoice) {
+    result = "Tie! Both players chose " + playerChoice + ".";
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    result = "You win! Rock beats scissors.";
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    result = "You win! Paper beats rock.";
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    result = "You win! Scissors beats paper.";
+  } else {
+    result = "You lose! " + computerChoice + " beats " + playerChoice + ".";
+  }
+
+  // display the outcome and the computer choice
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = `Result: ${result} <br> Computer Choice: ${computerChoice}`;
+}
