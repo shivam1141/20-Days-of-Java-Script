664ac7b7cc2d345bac869cb72ae20e06741deb7b
Shivam Kumar <89576755+shivam1141@users.noreply.github.com>
2023-01-19 21:11:48 +0530

    Add files via upload

---
+++ Day-17_Making a Clock/script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    var clock = document.getElementById("clock");
+    clock.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+setInterval(updateClock, 1000);
